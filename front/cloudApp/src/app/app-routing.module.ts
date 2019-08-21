0987b63c6410b3f842ee6180433967009c330abb
Gustavo Bejarano <ga.bejarano10@uniandes.edu.co>
2019-08-21 15:23:10 -0500

    agregar home, login, signup en el front

--- front/cloudApp/src/app/app-routing.module.ts
+++ front/cloudApp/src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {HomeComponent} from './componentes/home/home.component';
+import {LoginComponent} from './componentes/login/login.component';
+import {SignupComponent} from './componentes/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'signin',
+    component: LoginComponent,
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/cloudApp/src/app/app.module.ts
+++ front/cloudApp/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './componentes/login/login.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { NavegacionComponent } from './componentes/navegacion/navegacion.component';
+import { SignupComponent } from './componentes/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    NavegacionComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
