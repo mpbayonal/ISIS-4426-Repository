62f42cb42d02c7da821bdb44108d2d529ffdac32
paolabay <mp.bayonal@uniandes.edu.co>
2019-08-24 13:31:18 -0500

    se añadieron los modelos y sus relaciones

--- back/backApp/admin.py
+++ back/backApp/admin.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Proyecto
+from .models import Empresa
+from .models import Diseño
+
 
 # Register your models here.
+admin.site.register(Proyecto)
+admin.site.register(Empresa)
+admin.site.register(Diseño)
