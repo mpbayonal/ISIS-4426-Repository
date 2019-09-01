f123397da5b2e85c68d4f38b33883c2e805071b3
paolabay <mp.bayonal@uniandes.edu.co>
2019-08-31 19:31:36 -0500

    se corrigio un error en las urls

--- back/backApp/urls.py
+++ back/backApp/urls.py
@@ -14,11 +14,11 @@ urlpatterns = [
 
     url(r'^getdata/', views.get_data),
     path('proyectos/', views.ListProyecto.as_view()),
-    path('<int:pk>/', views.DetailProyecto.as_view()),
+    path('proyectos/<int:pk>/', views.DetailProyecto.as_view()),
     path('disenos/', views.ListDiseno.as_view()),
-    path('<int:pk>/', views.DetailDiseno.as_view()),
+    path('disenos/<int:pk>/', views.DetailDiseno.as_view()),
     path('empresas/', views.ListEmpresa.as_view()),
-    path('<int:pk>/', views.DetailEmpresa.as_view()),
+    path('empresas/<int:pk>/', views.DetailEmpresa.as_view()),
 
 
     # url(r'^login/', views.login),
