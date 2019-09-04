1025a0b2c20dad0f66be9cdb0b3feccd81c93710
paolabay <mp.bayonal@uniandes.edu.co>
2019-09-03 20:23:28 -0500

    se añadio el apellido al modelo del diseño

--- back/backApp/models.py
+++ back/backApp/models.py
@@ -35,6 +35,7 @@ class Proyecto(models.Model):
 class Diseno(models.Model):
 
     nombre = models.CharField(max_length=500)
+    apellido = models.CharField(max_length=500)
     email = models.CharField(max_length=500)
     estado = models.BooleanField()
     fecha = models.DateTimeField()
