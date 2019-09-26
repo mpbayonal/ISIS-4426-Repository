9424bbe5463a3619476306de8214f7c2b0024679
JairoBm13 <j.bautista.m13@outlook.com>
2019-09-26 17:16:49 -0500

    make celery work with the correspondent ip

--- back/back/celery.py
+++ back/back/celery.py
@@ -1,15 +1,20 @@
+from __future__ import absolute_import
+
 import os
 from celery import Celery
 from django.conf import settings
 
+import dotenv
+
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'back.settings')
+dotenv.read_dotenv()
 app = Celery('back')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
 app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.autodiscover_tasks(settings.INSTALLED_APPS)
 
 
 @app.task(bind=True)
