9aaa0af0e16b5c37f562fdf0112c1d0b276a6d8e
JairoBm13 <j.bautista.m13@outlook.com>
2019-10-23 16:52:56 -0500

    save time on dynamo and correct worker

--- backApp/tasks.py
+++ backApp/tasks.py
@@ -2,7 +2,7 @@ import datetime
 import io
 import os
 
-from boto3 import client
+from boto3 import client, resource
 from celery.decorators import task
 from celery.task.schedules import crontab
 from celery.utils.log import get_task_logger
@@ -13,6 +13,21 @@ from .models import Diseno
 
 logger = get_task_logger(__name__)
 
+dynamodb = boto3.resource(
+    'dynamodb',
+    region_name='us-east-1',
+    aws_access_key_id=settings.AWS_ACCESS_KEY,
+    aws_secret_access_key=settings.AWS_SECRET_KEY
+)
+
+connection = client(
+    'ses',
+    'us-east-1',
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_KEY')
+)
+
+dynamodb_table = dynamodb.Table('modelo-c')
 
 @task(
     name="send_feedback_email_task",
@@ -21,51 +36,45 @@ logger = get_task_logger(__name__)
 def process_image_and_send_mail(id):
     
     start = datetime.datetime.utcnow()
-    disenos = Diseno.objects.filter(estado="No Procesado")
-    canti = 0
-    for diseno in disenos:
-        startDiseno = datetime.datetime.utcnow()
-        img = Image.open(diseno.archivo, "r")
-        imgResize = img.resize((800, 600), Image.ANTIALIAS)
-        draw = ImageDraw.Draw(imgResize)
-        draw.text((0, 580), "{0} {1} - {2}".format(
-            diseno.nombre, diseno.apellido, diseno.fecha), (0, 0, 0))
-        tempfile_io = io.BytesIO()
-        imgResize.save(tempfile_io, format='JPEG')
-        image_file = InMemoryUploadedFile(
-            tempfile_io, None, diseno.archivo.name, 'image/jpeg', tempfile_io.getbuffer().nbytes, None)
-        diseno.url_archivo_modificado.save(diseno.archivo.name, image_file)
-        diseno.estado = "Disponible"
-        diseno.save()
+    diseno = Diseno.objects.filter(id=id)[:1].get()
+    img = Image.open(diseno.archivo, "r")
+    imgResize = img.resize((800, 600), Image.ANTIALIAS)
+    draw = ImageDraw.Draw(imgResize)
+    draw.text((0, 580), "{0} {1} - {2}".format(
+        diseno.nombre, diseno.apellido, diseno.fecha), (0, 0, 0))
+    tempfile_io = io.BytesIO()
+    imgResize.save(tempfile_io, format='JPEG')
+    image_file = InMemoryUploadedFile(
+        tempfile_io, None, diseno.archivo.name, 'image/jpeg', tempfile_io.getbuffer().nbytes, None)
+    diseno.url_archivo_modificado.save(diseno.archivo.name, image_file)
+    diseno.estado = "Disponible"
+    diseno.save()
 
-        connection = client(
-            'ses',
-            'us-east-1',
-            aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
-            aws_secret_access_key=os.getenv('AWS_SECRET_KEY')
-        )
-
-        response = connection.send_email(
-            Destination={
-                'ToAddresses': [diseno.email]
-            },
-            Message={
-                'Body': {
-                    'Text': {
-                        'Charset': 'UTF-8',
-                        'Data': 'Tus diseños ya están disponibles',
-                    },
-                },
-                'Subject': {
+    response = connection.send_email(
+        Destination={
+            'ToAddresses': [diseno.email]
+        },
+        Message={
+            'Body': {
+                'Text': {
                     'Charset': 'UTF-8',
-                    'Data': 'DesignMatch: Tus diseños ya están disponibles',
+                    'Data': 'Tu diseño ya están disponible\n \
+                    http://d1tprqpr49zo0i.cloudfront.net/empresa/proyectos/diseños/'+diseno.id,
                 },
             },
-            Source='je.bautista10@uniandes.edu.co',
-        )
-        canti = canti + 1
-        enddiseno = datetime.datetime.utcnow()
-        print((enddiseno-startDiseno).total_seconds())
+            'Subject': {
+                'Charset': 'UTF-8',
+                'Data': 'DesignMatch: Tus diseños ya están disponibles',
+            },
+        },
+        Source='je.bautista10@uniandes.edu.co',
+    )
     end = datetime.datetime.utcnow()
-    print(canti/60*(end-start).total_seconds())
+    dynamodb_table.put_item(
+            Item={
+                'origen': diseno.id,
+                'fecha': str(datetime.datetime.utcnow()),
+                'tiempo': (end-start).total_seconds()
+            }
+        )
     return (end-start).total_seconds()
