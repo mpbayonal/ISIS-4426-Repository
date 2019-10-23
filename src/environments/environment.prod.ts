8a67275c2d549a8a6c43ccf02e1e3cf92694d8b7
JairoBm13 <j.bautista.m13@outlook.com>
2019-10-23 04:44:12 -0500

    no more port

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backend_url: 'http://web-c-lb-472844658.us-east-1.elb.amazonaws.com:8000'
+  backend_url: 'http://web-c-lb-472844658.us-east-1.elb.amazonaws.com'
 };
