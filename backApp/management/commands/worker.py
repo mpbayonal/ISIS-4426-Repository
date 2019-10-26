e48c07988f2d1afb0236153127490c03e6af7682
JairoBm13 <j.bautista.m13@outlook.com>
2019-10-26 17:56:03 -0500

    cuenta

--- backApp/management/commands/worker.py
+++ backApp/management/commands/worker.py
@@ -35,34 +35,43 @@ class Command(BaseCommand):
             aws_access_key_id=os.getenv('AWS_GUSTAVO_ID'),
             aws_secret_access_key=os.getenv('AWS_GUSTAVO_SECRET')
         )
+        base = 0
+        cuantos = 0;
+        ya = false
         while True:
-            sqs = client('sqs', 'us-east-1')
             response = sqs.receive_message(
                 QueueUrl='https://sqs.us-east-1.amazonaws.com/547712166517/designmatch-d')
             if 'Messages' in response:
+                inicio = datetime.datetime.utcnow() + base
                 for message in response['Messages']:
-                    start = datetime.datetime.utcnow()
+                    end = datetime.datetime.utcnow()
+                    dynamodb.put_item(
+                        TableName='modelo-d',
+                        Item={
+                            'origen': {'S': str(diseno_id)},
+                            'fecha': {'S': str(datetime.datetime.utcnow())},
+                            'tiempo': {'N': str((end-inicio).total_seconds())}
+                        }
+                    )
                     diseno_id = message['Body']
                     diseno = Diseno.get_id(diseno_id)['Items'][0]
                     s3 = resource('s3')
                     s3.Bucket(s3_images_bucket).download_file(
-                        diseno['archivo'], '/tmp/' +
-                            diseno['archivo'].replace('noProcesado/', ''))
-                    img=Image.open('/tmp/'+diseno['archivo'].replace('noProcesado/', ''), "r")
-                    imgResize=img.resize((800, 600), Image.ANTIALIAS)
-                    draw=ImageDraw.Draw(imgResize)
+                        diseno['archivo'], '/tmp/'+diseno['archivo'])
+                    img = Image.open('/tmp/'+diseno['archivo'], "r")
+                    imgResize = img.resize((800, 600), Image.ANTIALIAS)
+                    draw = ImageDraw.Draw(imgResize)
                     draw.text((0, 580), "{0} {1} {2}".format(
                         diseno['nombre'], diseno['apellido'], diseno['fecha']), (0, 0, 0))
-                    in_mem_file=io.BytesIO()
+                    in_mem_file = io.BytesIO()
                     imgResize.save(in_mem_file, format='JPEG')
-                    in_mem_file.seek(0)
-                    nombre=diseno['archivo'].replace(
+                    nombre = diseno['archivo'].replace(
                         'noProcesado/', 'disponible/')
                     try:
-                        file_object=resource('s3').Object(
+                        file_object = resource('s3').Object(
                             s3_images_bucket, nombre).last_modified
-                        file_root, file_ext=os.path.splitext(nombre)
-                        new_name='{0}{1}{2}{3}'.format(
+                        file_root, file_ext = os.path.splitext(nombre)
+                        new_name = '{0}{1}{2}{3}'.format(
                             file_root, '_', get_random_string(), file_ext)
                         resource('s3').Bucket(s3_images_bucket).put_object(
                             Body=in_mem_file,
@@ -96,7 +105,7 @@ class Command(BaseCommand):
                             diseno['email'],
                             'Disponible')
 
-                    response=connection.send_email(
+                    response = connection.send_email(
                         Destination={
                             'ToAddresses': [diseno['email']]
                         },
@@ -113,19 +122,19 @@ class Command(BaseCommand):
                                 'Data': 'DesignMatch: Tus diseños ya están disponibles',
                             },
                         },
-                        Source='je.bautista10@uniandes.edu.co',
-                    )
-                    end=datetime.datetime.utcnow()
-                    dynamodb.put_item(
-                        TableName='modelo-d',
-                        Item={
-                            'origen': {'S': str(diseno_id)},
-                            'fecha': {'S': str(datetime.datetime.utcnow())},
-                            'tiempo': {'N': str((end-start).total_seconds())}
-                        }
+                        Source='ga.bejarano10@uniandes.edu.co',
                     )
                     # Let the queue know that the message is processed
-                    sqs=resource('sqs', 'us-east-1')
-                    message=sqs.Message(
+                    sqs = resource('sqs')
+                    message = sqs.Message(
                         'https://sqs.us-east-1.amazonaws.com/547712166517/designmatch-d', message['ReceiptHandle'])
                     message.delete()
+                    if ya == False and (end-inicio).total_seconds >= 60:
+                        dynamodb.put_item(
+                            TableName='cuantos-d',
+                            Item={
+                                'cuantos': {'N': str(cuenta)}
+                            }
+                        )
+                        ya = True
+                base = end-inicio
